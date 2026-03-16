d54591c486b40e9a7f87360789f4984496091d77
npaul-2 <npaul2@csub.edu>
2026-03-16 10:42:54 -0700

    Added contact page layout

--- src/app/about.jsx
+++ src/app/about.jsx
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   heading: {
-    //fontweight: "bold",
+    fontWeight: "bold",
     fontSize: 30,
     justifyContent: "center",
   },
--- src/app/contact.jsx
+++ src/app/contact.jsx
@@ -1,12 +1,21 @@
 import { Link } from 'expo-router'
+import { useState } from 'react'
 import { ScrollView, StyleSheet } from 'react-native'
 import Footer from '../components/Footer'
 import Spacer from '../components/Spacer'
+import ThemedButton from '../components/ThemedButton'
 import ThemedText from '../components/ThemedText'
+import ThemedTextInput from '../components/ThemedTextInput'
 import ThemedView from '../components/ThemedView'
-import TitleCard from '../components/TitleCard'
 
 const Contact = () => {
+  const [name, setName]= useState('')
+  const [email, setEmail]= useState('')
+  const [msg, setMsg]= useState('')
+
+  const handleSubmit = () => {
+    console.log('contact info submitted', name, msg, email)
+  }
 
   return (
     <ThemedView style={{flex: 1}}>
@@ -14,12 +23,42 @@ const Contact = () => {
     <ThemedView style={styles.container}>
 
       <Spacer/>
-      <TitleCard txt='Contact Form'>
-        <ThemedText>
-          add one that works{"\n"}
-          plz{"\n"}  
-        </ThemedText>    
-      </TitleCard>
+
+      <ThemedText title={true} style={styles.heading}>
+         Contact Form {"\n"}
+      </ThemedText>
+      <Spacer/>
+
+      <ThemedTextInput style={{width: '80%', marginBottom: 20}}
+      placeholder='Name'
+      onChangeText={setName}
+      value={name}
+      ></ThemedTextInput>
+
+      <ThemedTextInput style={{width: '80%', marginBottom: 20}}
+      placeholder='Email'
+      keyboardType="email-address"
+      onChangeText={setEmail}
+      value={email}
+      ></ThemedTextInput>
+
+      <ThemedTextInput style={{
+        width: '80%',
+        height: 200,
+        marginBottom: 20,
+        padding: 15,
+        textAlignVertical: 'top'
+        }}
+      placeholder='Message'
+      onChangeText={setMsg}
+      value={msg}
+      multiline={true}
+      numberOfLines={7}
+      ></ThemedTextInput>
+
+      <ThemedButton onPress={handleSubmit}>
+        <ThemedText>Submit</ThemedText>
+      </ThemedButton>
 
       <Spacer/>
 
@@ -59,5 +98,11 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     borderBottomColor: "blue",
     color: "blue",
+  },
+  heading: {
+    fontWeight: "bold",
+    fontSize: 30,
+    alignItems: "center", 
+    justifyContent: "center",
   },
 })
